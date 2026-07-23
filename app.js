06035ed02514bdfb43b465a18e71f1451aafdf5b
Ky0y4 <88522509+Ky0y4@users.noreply.github.com>
2026-07-23 15:20:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const canvas = document.getElementById("renderCanvas");
+
+const engine = new BABYLON.Engine(canvas, true);
+
+const createScene = async function () {
+
+    const scene = new BABYLON.Scene(engine);
+
+    const camera =
+        new BABYLON.ArcRotateCamera(
+            "camera",
+            -Math.PI/2,
+            Math.PI/2.5,
+            3,
+            BABYLON.Vector3.Zero(),
+            scene
+        );
+
+    camera.attachControl(canvas,true);
+
+    const light =
+        new BABYLON.HemisphericLight(
+            "light",
+            new BABYLON.Vector3(0,1,0),
+            scene
+        );
+
+    const splat = new BABYLON.GaussianSplattingMesh("splat", null, scene);
+    await splat.loadFileAsync("./vrlab densed 1m.spz");
+    await scene.createDefaultXRExperienceAsync({
+        uiOptions: {
+            sessionMode: "immersive-ar"
+        }
+    }); 
+
+    return scene;
+};
+
+(async () => {
+
+    const scene = await createScene();
+
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
+
+})();
+
+window.addEventListener("resize", function () {
+    engine.resize();
+});
+
